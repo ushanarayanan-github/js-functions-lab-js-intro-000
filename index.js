bd3545cff590bcb90a5b0c6aa5f012337fb5ed2d
creative-buffer-9578 <cmuusha@gmail.com>
2020-06-27 03:35:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function happyCustomHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return `It's ${days} days until ${holiday}!`
+  return "It's " + days + " days until " + holiday + "!"
 }
